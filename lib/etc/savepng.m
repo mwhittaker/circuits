1bdeb14796b87374e10830533be802be62c6e829
Michael Whittaker <mjw297@cornell.edu>
2013-10-12 17:33:23 -0400

    Fixed savepng bug.
    I borrowed savepng from an earlier script I wrote and forgot to change
    something. Every image was being saved in a "../img/" directory which didn't
    always exist.

--- lib/etc/savepng.m
+++ lib/etc/savepng.m
@@ -2,5 +2,5 @@ function savepng(fig, filename)
 % Saves a figure in png format.
 % @param fig A figure handle.
 % @param filename The filename of the image.
-    saveas(fig, strcat('../img/', filename), 'png')
+    saveas(fig, filename, 'png')
 end
