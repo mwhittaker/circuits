37aaa84404798ab15ff457541af495340216246b
Michael Whittaker <mjw297@cornell.edu>
2013-11-16 00:21:06 -0500

    Minor mods.

--- lib/circuits/parallel.m
+++ lib/circuits/parallel.m
@@ -2,5 +2,5 @@ function ret = parallel(v)
 % Return parallel impedance
 % @param v Vector of impedences
 % @return Equivalent parallel impedance. 
-    ret = 1 ./ sum(1 ./ v)
+    ret = 1 ./ sum(1 ./ v);
 end
